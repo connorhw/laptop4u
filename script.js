aa45aaac7f3a79b7d11245f0eaff37947958992d
Connor <connorwong93@gmail.com>
2019-10-25 14:14:32 -0700

    1st MVP submission - allows user to select a manufacturer, displays 10 laptops from the API from the selected manufacturer.

--- script.js
+++ script.js
@@ -3,8 +3,13 @@
 const apiKey = '1TGJYsFjOVTW6vG1Qsx2e2i8';
 const baseURL = 'https://api.bestbuy.com/v1/products((categoryPath.id=abcat0502000))';
 
-function getLaptops() {
-    const url = 'https://api.bestbuy.com/v1/products((categoryPath.id=abcat0502000))?apiKey=1TGJYsFjOVTW6vG1Qsx2e2i8&sort=description.asc&show=categoryPath.id,categoryPath.name,description,details.name,features.feature,manufacturer,name,regularPrice,thumbnailImage&format=json';
+function getBBLaptops(manuf) {
+    /*
+    const params = {
+        manufacturer: manuf
+    };
+    */
+    const url = 'https://api.bestbuy.com/v1/products(manufacturer='+manuf+'&(categoryPath.id=abcat0502000))?apiKey=1TGJYsFjOVTW6vG1Qsx2e2i8&sort=description.asc&show=categoryPath.id,categoryPath.name,description,details.name,features.feature,manufacturer,name,regularPrice,thumbnailImage&format=json';
     fetch(url)
         .then(response => {
             if (response.ok) {
@@ -18,11 +23,29 @@ function getLaptops() {
 
 function displayResults(responseJson) {
     console.log(responseJson);
+    $('.bb-laptops-list').empty();
+    for (let i=0; i<responseJson.products.length; i++) {
+        $('.bb-laptops-list').append(
+            `<li>
+            <p>${responseJson.products[i].name}</p>
+            <div>${responseJson.products[i].manufacturer}</div>
+            </li>`
+        )
+    };
+    $('.results').removeClass('hidden');
 }
 
 function watchForm() {
+    
+    $('.form').submit(function(event) {
+        event.preventDefault();
+       const manuf = $('#js-manuf').val();
+
+        getBBLaptops(manuf);
+    });
+    
     console.log('On the lookout!');
-    getLaptops();
+    
 }
 
 $(function() {
