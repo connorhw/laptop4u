0730f527c0e9006be8343cb52f7b52fd62ba72e1
Connor Wong <connorwong93@gmail.com>
2019-11-19 23:07:24 -0800

    implements storage type criteria in search

--- script.js
+++ script.js
@@ -7,22 +7,19 @@ let laptopData;
 function getBBLaptops(manuf, maxPrice, storage) {
     const url = 'https://api.bestbuy.com/v1/products(manufacturer='+manuf+'&(categoryPath.id=abcat0502000)&condition=new&regularPrice<='+maxPrice+')'+
     '?apiKey=1TGJYsFjOVTW6vG1Qsx2e2i8&sort=description.asc&show=all&'+
-    'pageSize=10&format=json';
+    'pageSize=100&format=json';
     fetch(url)
         .then(response => {
             if (response.ok) {
-                console.log('response is good');
+                //console.log('response is good');
                 return response.json();
             }
             throw new Error(response.statusText);
         })
         .then(responseJson => {
+            //console.log(responseJson);
             laptopData = responseJson.products;
-            /* pick storage type here */
-            
-            let storage = "SDD"; //hard coding in value for testing purposes
-            var newArray = laptopData.filter(item => item.details["Storage Type"] === storage);
-            
+            var newArray = laptopData.filter(item => item.details.find(x => x.name === "Storage Type").value === storage); //filters storage type
             displayResults(newArray);
         });
 }
@@ -30,12 +27,12 @@ function getBBLaptops(manuf, maxPrice, storage) {
 function displayResults(newArray) {
     console.log(newArray);
     $('.bb-laptops-list').empty();
-    for (let i=0; i<laptopData.length; i++) { 
+    for (let i=0; i<newArray.length; i++) { 
         $('.bb-laptops-list').append(
             `<li class="laptop-li" node=${i}>
-            <p>${laptopData[i].name}: $${laptopData[i].regularPrice}</p>
+            <p>${newArray[i].name}: $${newArray[i].regularPrice}</p>
             <button type="button" id="select-button">Choose</button>
-            <img src="${laptopData[i].thumbnailImage}"></br>
+            <img src="${newArray[i].thumbnailImage}"></br>
             </li>`
         )
     };
@@ -56,7 +53,7 @@ function displaySelectedLaptopListener() {
 
 function getSelectedLaptopInfo(index){
     $('.bb-selected-laptop').empty();
-    console.log(laptopData)
+    //console.log(laptopData)
     $(".bb-selected-laptop").append(
     `<img id ="full-laptop-image" src="${laptopData[index].image}" alt="Full image of computer">`
     )
@@ -70,19 +67,17 @@ function getSelectedLaptopInfo(index){
 }
 
 function watchForm() {
-    
     $('.form').submit(function(event) {
         event.preventDefault();
         const manuf = $('#js-manuf').val();
         const maxPrice =$('#js-price').val();
-        const storage = $('js-storage').val();
+        const storage = $('#js-storage').val();
         getBBLaptops(manuf, maxPrice, storage);
     });
-    
-    console.log('On the lookout!');
+    //console.log('On the lookout!');
 }
 
 $(function() {
-    console.log('App loaded successfully!');
+    //console.log('App loaded successfully!');
     watchForm();
 });
